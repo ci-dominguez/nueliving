841a2860ba50998908a7e11429c71e4a504419cc
Cristian <124216023+ci-dominguez@users.noreply.github.com>
2023-05-27 08:25:02 +0000

    AAAA

---
+++ utils/data.js
@@ -0,0 +1,44 @@
+const data = {
+    products: [
+      {
+        name: 'Post Storage Cabinet',
+        slug: 'post-storage-cabinet',
+        main: 'Furniture',
+        sub: 'Cabinets',
+        image: '/images/post_storage_cabinet.webp',
+        color: 'Smoked Oak',
+        price: 10,
+        rating: 4.5,
+        numReviews: 8,
+        countInStock: 20,
+        description:
+          'Inspired by brutalism, the Post Storage Cabinet plays with volumes to present a remarkable storage solution with elaborate details that draw you in. Made from FSC Mix-certified wood, the cabinet feature striped wood detailing in oak veneer that has been achieved through an intricate and time-consuming carpentry technique known as marquetry. Preserving the simple, artful expression of the striped front, the cabinet doors are opened and closed with a hidden mechanism to avoid any visible handles.',
+        itemno: 1104265719,
+        dimensions: 'W: 35.22in x H: 52.76in x D: 16.73in',
+        material:
+          'FSC Mix-certified smoked dark/light oak veneer with chipboard panels. Oiled Cabinet with two shelves.',
+        care: 'Wipe with a damp cloth. Do not leave the surface wet',
+      },
+      {
+        name: 'Crease Wool Cushion',
+        slug: 'crease-wool-cushion',
+        main: 'Textiles',
+        sub: 'Cushions',
+        image: '/images/crease_wool_cushion.webp',
+        color: 'Bone',
+        price: 27,
+        rating: 3.5,
+        numReviews: 18,
+        countInStock: 3,
+        description:
+          'Drawing inspiration from the clean lines found in modern architecture, the Crease Wool Cushion has a soft, understated expression. Handwoven from New Zealand wool, the front of the cushion is a mix of high pile and flat woven wool which is tufted in a minimalistic pattern for a decorative, graphic expression. The back of the cushion is kept flat woven. Adding to its tactility, the cushion is completed with intricate top stitched details along the edges.',
+        itemno: 1104264622,
+        dimensions: 'W: 19.69in x H: 19.69in',
+        material:
+          '100% New Zealand wool. Feather and down filling Hand-woven and hand-tufted',
+        care: 'Dry clean only',
+      },
+    ],
+  };
+  
+  export default data;
